751bf4337c3cfc78996dc5db472efd9ee6c10f20
Connor Nichols <cnichols9@huskers.unl.edu>
2022-01-20 13:05:23 -0600

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Connor Nichols on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
